32c568ad42a0c4bc0380421846d8630f1b54472c
Chu Nguyen Quang Thai <1955010189@kientruchanoi.edu.vn>
2026-05-08 19:03:13 +0700

    second

--- src/main/java/com/thaichu/ADFA/PlacesController.java
+++ src/main/java/com/thaichu/ADFA/PlacesController.java
@@ -2,6 +2,8 @@ package com.thaichu.ADFA;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/places")
 @CrossOrigin(origins = "*")
 public class PlacesController {
-    @Autowired
-    private PlacesRepo placesRepo;
 
     @GetMapping("/getAllPlaces")
-    public List<Places> getAllPlaces() {
-        return placesRepo.findAll();
+    public List<Map<String, Object>> getAllPlaces() {
+        return List.of(
+            Map.of("id", 1, "name", "Hoi An", "rating", 4.5, "imageUrl", "https://example.com/hoian.jpg"),
+            Map.of("id", 2, "name", "Sai gon", "rating", 4.7, "imageUrl", "https://example.com/saigon.jpg")
+        );
     }
 }
